e97db4ff18c271287fdba47c072b756eedf44a55
ceasar28 <emmanuelugochukwuekete@gmail.com>
2023-11-19 17:43:30 +0100

    ssl connect

--- src/models/index.js
+++ src/models/index.js
@@ -1,20 +1,28 @@
-//importing modules
-// https://medium.com/@rachealkuranchie/node-js-authentication-with-postgresql-sequelize-and-express-js-20ae773da4c9
 const { Sequelize, DataTypes } = require("sequelize");
+const dotenv = require("dotenv").config();
 
 //Database connection with dialect of postgres specifying the database we are using
 //port for my database is 5433
 //database name is discover
 const sequelize = new Sequelize(
-  `postgres://postgres:1998born@localhost:5432/ezigbo`,
-  { dialect: "postgres" }
+  process.env.DB_URL,
+  //`postgres://postgres:1998born@localhost:5432/ezigbo`,
+  {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // This setting allows you to bypass SSL verification (use carefully)
+      },
+    },
+  }
 );
 
 //checking if connection is done
 sequelize
   .authenticate()
   .then(() => {
-    console.log(`Database connected to discover`);
+    console.log(`Database connected to ezigbo`);
   })
   .catch((err) => {
     console.log(err);
